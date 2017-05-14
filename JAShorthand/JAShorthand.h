ac7820371920f36262552a7bebff387bcd0b2be3
Julio Alorro <julio.alorro@ffuf.de>
2017-05-14 17:53:50 +0800

    Initial Commit

---
+++ JAShorthand/JAShorthand.h
@@ -0,0 +1,19 @@
+//
+//  JAShorthand.h
+//  JAShorthand
+//
+//  Created by Julio Alorro on 5/14/17.
+//  Copyright © 2017 Some Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JAShorthand.
+FOUNDATION_EXPORT double JAShorthandVersionNumber;
+
+//! Project version string for JAShorthand.
+FOUNDATION_EXPORT const unsigned char JAShorthandVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JAShorthand/PublicHeader.h>
+
+
