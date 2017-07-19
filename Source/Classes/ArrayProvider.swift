3e715f9238fe06cf53cce04e0deca6b07aae184c
Julio Alorro <alorro3@gmail.com>
2017-07-19 13:48:32 +0800

    Removed extra LICENSE. Added add/remove functions in ArrayProvider

--- Source/Classes/ArrayProvider.swift
+++ Source/Classes/ArrayProvider.swift
@@ -18,10 +18,24 @@ public final class ArrayProvider<Type>: DataProviding {
         return self._objects
     }
 
-    public func numberOfSections() -> Int { return 1 }
+    public func numberOfSections() -> Int {
+        return 1
+    }
 
-    public func numberOfRows(in section: Int) -> Int { return self.objects.count }
+    public func numberOfRows(in section: Int) -> Int {
+        return self.objects.count
+    }
 
-    public func object(at indexPath: IndexPath) -> Type { return self.objects[indexPath.row] }
+    public func object(at indexPath: IndexPath) -> Type {
+        return self.objects[indexPath.row]
+    }
+
+    public func addObject(_ object: Type) {
+        self._objects.append(object)
+    }
+
+    public func removeObject(_ object: Type, at indexPath: IndexPath) {
+        self._objects.remove(at: indexPath.row)
+    }
 
 }
