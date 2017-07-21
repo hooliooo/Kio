ff405d7e3bdb353df5ef10ed3e3bf1da3f3a557f
Julio Alorro <alorro3@gmail.com>
2017-07-21 22:44:34 +0800

    Created new directories

---
+++ Source/Coordinators/TabBarCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  Rapid
+//  Copyright © Julio Alorro 2017
+//
+//  Licensed under the MIT license. See LICENSE file.
+//
+//
+
+import UIKit
+
+public protocol TabBarCoordinator: Coordinator {
+
+    /**
+     Array of TabCoordinators managed by the TabBarCoordinator
+    */
+    var tabs: [AnyTabCoordinator] { get set }
+
+    /**
+     Type erasing method for TabCoordinator
+    */
+    public func degenericize<T: TabCoordinator>(_ coordinator: T) -> AnyTabCoordinator
+}
+
+public extension TabBarCoordinator {
+
+    public func degenericize<T: TabCoordinator>(_ coordinator: T) -> AnyTabCoordinator {
+        return AnyTabCoordinator(coordinator)
+    }
+}
