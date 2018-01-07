bd5e7cc8e329d64ebe404a3cf3ff10a37b1b5b23
Julio Alorro <alorro3@gmail.com>
2018-01-08 00:03:58 +0800

    JAArchiveManager changes made
    Added fileExisis method
    
    Added initializer
    
    Refined documentation in JAArchiveManager

--- Source/Classes/JAArchiveManager.swift
+++ Source/Classes/JAArchiveManager.swift
@@ -8,39 +8,87 @@
 
 import Foundation
 
+/**
+ The JAArchiveManager class is responsible for archiving/unarchiving Codable objects using NSKeyedArchiver and NSKeyedUnarchiver.
+*/
 public class JAArchiveManager: JAObject {
 
     // MARK: Enums
+    /**
+     An Archive Error is an error that may occur during archiving/unarchiving of a Codable object.
+    */
     public enum ArchiveError: Swift.Error {
+        /**
+         Represents an error during archiving.
+        */
         case archiveError(String)
+
+        /**
+         Represents an error during unarchiving.
+        */
         case unarchiveError(String)
     }
 
+    // MARK: Initializer
+    /**
+     Initializer for a JAArchiveManager instance.
+     - parameter directory: The directory where files will be archived/unarchived. Default is the Documents Directory.
+     - parameter domainMask: The domain mask where where files will be archived/unarchived. Default is User Domain Mask.
+     - parameter fileManager: The FileManager instance to be used. Default is the FileManager.default singleton.
+    */
+    public init(
+        directory: FileManager.SearchPathDirectory = FileManager.SearchPathDirectory.documentDirectory,
+        domainMask: FileManager.SearchPathDomainMask = FileManager.SearchPathDomainMask.userDomainMask,
+        fileManager: FileManager = FileManager.default
+    ) {
+        self.directory = directory
+        self.domainMask = domainMask
+        self.fileManager = fileManager
+        super.init()
+    }
+
+    // MARK: Stored Property
+    /**
+     Directory where all archiving/unarchiving will occur.
+    */
+    private final let directory: FileManager.SearchPathDirectory
+
+    /**
+     Domain where all archiving/unarchiving will occur.
+    */
+    private final let domainMask: FileManager.SearchPathDomainMask
+
+    /**
+     FileManager instance.
+    */
+    private final let fileManager: FileManager
+
     // MARK: Computed Properties
     /**
-     The URL to the Documents Directory
+     The URL to the directory where archiving and unarchiving will occur.
     */
-    private final var documentsDirectoryURL: URL {
-        return FileManager.default.urls(
-            for: FileManager.SearchPathDirectory.documentDirectory,
-            in: FileManager.SearchPathDomainMask.userDomainMask
+    public final var directoryURL: URL {
+        return self.fileManager.urls(
+            for: self.directory,
+            in: self.domainMask
         )[0]
     }
 
     // MARK: Instance Methods
     /**
-     This method saves the Encodable object to the documents directory of the device.
-     First, JAArchiveManager encodes the object with a JSONEncoder, then saves the Data instance to the disk
-     with the specified file name in the Documents Directory.
+     Saves the Codable object to the directory specified by the directoryURL property.
+     First, the object is encoded as Data with a JSONEncoder, then saved to the directory
+     specified by the directoryURL property.
+
      Throws an error if something went wrong with the encoding/archiving.
-     - parameter object: The object to be persisted to the disk
-     - parameter fileName: The file name of the object when it is persisted to the disk
+     - parameter object: The object to be persisted to the disk.
+     - parameter fileName: The file name of the object when it is persisted to the disk.
     */
-    public func archive<T: Encodable>(object: T, asFileName fileName: String) throws {
+    public func archive<T: Codable>(object: T, as fileName: String) throws {
         let encoder: JSONEncoder = JSONEncoder()
         let data: Data = try encoder.encode(object)
 
-        let path: String = self.documentsDirectoryURL.appendingPathComponent(fileName).path
+        let path: String = self.directoryURL.appendingPathComponent(fileName).path
 
         switch NSKeyedArchiver.archiveRootObject(data, toFile: path) {
             case true:
@@ -52,20 +100,29 @@ public class JAArchiveManager: JAObject {
     }
 
     /**
-     This method creates an instance of the Decodable object from the data used in the specified file name.
-     First, JAArchiveManager unarchives a Data instance from the file in the Documents Directory then decodes the Data with
-     a JSONDecoder.
-     Throws an error if something went wrong with the unarchiving/decoding
+     Creates a Codable object from the specified file.
+     First, The Data in unarchived the file in the directory specified by the directoryURL
+     then decoded with a JSONDecoder and returns the Codable object.
+
+     Throws an error if something went wrong with the unarchiving/decoding.
      - parameter fileName: The file name of the object to be unarchived.
     */
-    public func unarchive<T: Decodable>(fromFileName fileName: String) throws -> T {
+    public func unarchive<T: Codable>(from fileName: String) throws -> T {
 
-        let path: String = self.documentsDirectoryURL.appendingPathComponent(fileName).path
+        let path: String = self.directoryURL.appendingPathComponent(fileName).path
 
         guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: path) as? Data
             else { throw ArchiveError.unarchiveError("Unarchiving couldn't typecast to Data") }
         let decoder: JSONDecoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
+    }
+
+    /**
+     Checks if file exist in the direcorty specified by the directoryURL. Returns a Bool.
+    */
+    public final func fileExists(_ fileName: String) -> Bool {
+        let path: String = self.directoryURL.appendingPathComponent(fileName).path
+        return self.fileManager.fileExists(atPath: path)
     }
 
 }
