bc2504ed1a7c2075c404c6be828b857256fe4392
hooliooo <alorro3@gmail.com>
2018-09-17 10:20:33 +0200

    Changed header file name in Support directory

---
+++ Source/Support/Kio.h
@@ -0,0 +1,19 @@
+//
+//  Kio.h
+//  Kio
+//
+//  Created by Julio Alorro on 5/14/17.
+// 
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kio.
+FOUNDATION_EXPORT double KioVersionNumber;
+
+//! Project version string for Kio.
+FOUNDATION_EXPORT const unsigned char KioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kio/PublicHeader.h>
+
+
